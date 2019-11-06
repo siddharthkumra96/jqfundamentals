0f02c4ef59ea643b347f302f42f751f5e27b8415
siddharthkumra@vinsol.com <siddharthkumra@vinsol.com>
2019-11-06 17:56:55 +0530

    specials updated

--- exercises/js/specials.js
+++ exercises/js/specials.js
@@ -1,29 +1,37 @@
-var cachedResponse;
-var $specials = $("#specials");
-var $target = $("<div/>");
-$specials.append($target);
-var $select = $specials.find("select");
-$select.change(function(){
-  let $this = $(this);
-  let val = $this.val();
-  if(!cachedResponse){
+class DynamicSelect{
+  constructor(id){
+    this.$specials = $("#"+id);
+    this.cachedResponse;
+    this.$target = $("<div/>");
+    this.$specials.append(this.$target);
+  }
+  addListeners(){
+    let $select = this.$specials.find("select");
+    $select.change(this.getData.bind(this));
+    this.$specials.find(".buttons").remove();
+  }
+  getData(e){
+    let $option = $(e.currentTarget);
+    let val = $option.val();
+  if(!this.cachedResponse){
     $.get("/data/specials.json").done((res)=> {
-      cachedResponse=res;
-      fillTarget(res[val]);
+      this.cachedResponse=res;
+      this.updateTarget(res[val]);
     });
   }
   else{
-    fillTarget(cachedResponse[val]);
+    this.updateTarget(this.cachedResponse[val]);
   }
-});
-function fillTarget(values){
-  let html = "";
-  let title= "<h3>"+values.title+"</h3>";
-  let text = "<p>"+values.text+"</p>";
-  let image = "<img src=\""+values.image+"\"/>";
-  html = title + text + image;
-  $target.html(html);
-  $target.css("color", values.color);
+  }
+  updateTarget(values){
+    let html = "";
+    let title= "<h3>"+values.title+"</h3>";
+    let text = "<p>"+values.text+"</p>";
+    let image = "<img src=\""+values.image+"\"/>";
+    html = title + text + image;
+    this.$target.html(html);
+    this.$target.css("color", values.color);
+  }
 }
-$specials.find(".buttons").remove();
-
+var specials = new DynamicSelect("specials");
+specials.addListeners();
