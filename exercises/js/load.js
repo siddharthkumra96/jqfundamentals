4741bd835c59afbfc1a0b834247bf7d7a7671d1b
siddharthkumra@vinsol.com <siddharthkumra@vinsol.com>
2019-11-06 17:41:12 +0530

    load.js updated

--- exercises/js/load.js
+++ exercises/js/load.js
@@ -1,14 +1,24 @@
-let $blog = $("#blog");
-let $h3s = $blog.find("h3");
-$h3s.each(function(i, h3){
-  let $h3 = $(h3);
-  let $h3Container = $("<div/>");
-  $h3.data("container", $h3Container);
-  $h3Container.insertAfter($h3);
-  }).click(function(e){
+class LoadBlogData{
+  constructor(blogId){
+    this.$blog = $("#"+blogId);
+  }
+  addListeners(){
+    let $h3s = this.$blog.find("h3");
+    $h3s.each(function(i, h3){
+    let $h3 = $(h3);
+    let $h3Container = $("<div/>");
+    $h3.data("container", $h3Container);
+    $h3Container.insertAfter($h3);
+    }).click(this.loadPostData.bind(this));
+  }
+  loadPostData(e){
     e.preventDefault();
-    let $this = $(this);
-    let $container = $this.data("container");
-    let id  = "#" + $this.find("a").attr("href").split("#")[1];
-    $container.load("data/blog.html " + id); //space to filter with the provided id
-  });
+    let $targetH3 = $(e.currentTarget);
+    
+    let $container = $targetH3.data("container");
+    let id  = "#" + $targetH3.find("a").attr("href").split("#")[1];
+    $container.load("data/blog.html " + id);
+  }
+}
+var blog = new LoadBlogData("blog");
+blog.addListeners();
