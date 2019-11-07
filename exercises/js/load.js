0f9da4d4ca0cf43e88adc5740bedb40fd490aeb5
siddharthkumra@vinsol.com <siddharthkumra@vinsol.com>
2019-11-07 16:23:12 +0530

    load.js indentation fix

--- exercises/js/load.js
+++ exercises/js/load.js
@@ -1,21 +1,28 @@
-class LoadBlogData{
-  constructor(blogId){
-    this.$blog = $("#"+blogId);
+class LoadBlogData {
+  constructor(blogId) {
+    this.$blog = $("#" + blogId);
   }
-  addListeners(){
+  addListeners() {
     let $h3s = this.$blog.find("h3");
-    $h3s.each(function(i, h3){
-    let $h3 = $(h3);
-    let $h3Container = $("<div/>");
-    $h3.data("container", $h3Container);
-    $h3Container.insertAfter($h3);
-    }).click(this.loadPostData.bind(this));
+    $h3s
+      .each(function(i, h3) {
+        let $h3 = $(h3);
+        let $h3Container = $("<div/>");
+        $h3.data("container", $h3Container);
+        $h3Container.insertAfter($h3);
+      })
+      .click(this.loadPostData.bind(this));
   }
-  loadPostData(e){
+  loadPostData(e) {
     e.preventDefault();
     let $targetH3 = $(e.currentTarget);
     let $container = $targetH3.data("container");
-    let id  = "#" + $targetH3.find("a").attr("href").split("#")[1];
+    let id =
+      "#" +
+      $targetH3
+        .find("a")
+        .attr("href")
+        .split("#")[1];
     $container.load("data/blog.html " + id);
   }
 }
