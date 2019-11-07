b2cf4658533b50493559539958c46bad92ae11b4
siddharthkumra@vinsol.com <siddharthkumra@vinsol.com>
2019-11-07 16:08:38 +0530

    inputHint.js indentation chnages

--- exercises/js/inputHint.js
+++ exercises/js/inputHint.js
@@ -1,28 +1,30 @@
-class InputHint{
-  constructor(id){
-    this.$input = $( "input[name='" + id + "']" );
-    this.$label = $($( "label[for='"+id+"']"));
+class InputHint {
+  constructor(id) {
+    this.$input = $("input[name='" + id + "']");
+    this.$label = $("label[for='" + id + "']");
     this.label_text = this.$label.text();
   }
-  addHint(){
+  addHint() {
     this.$input.val(this.label_text);
-    this.$input.addClass("hint").focus(this.onFocus.bind(this)).blur(this.onBlur.bind(this));
+    this.$input
+      .addClass("hint")
+      .focus(this.onFocus.bind(this))
+      .blur(this.onBlur.bind(this));
     this.$label.remove();
   }
-  onFocus(){
-    if(this.$input.val() === this.label_text && this.$input.hasClass("hint")){
-      this.$input.val("") ; 
+  onFocus() {
+    if (this.$input.val() === this.label_text && this.$input.hasClass("hint")) {
+      this.$input.val("");
       this.$input.removeClass("hint");
     }
   }
-  onBlur(){
-     if(!this.$input.val().trim()){
-       this.$input.val(this.label_text); 
-       this.$input.addClass("hint");
-      }
-      else{
-        this.$input.val(this.$input.val().trim());
-      }
+  onBlur() {
+    if (!this.$input.val().trim()) {
+      this.$input.val(this.label_text);
+      this.$input.addClass("hint");
+    } else {
+      this.$input.val(this.$input.val().trim());
+    }
   }
 }
 let input = new InputHint("q");
