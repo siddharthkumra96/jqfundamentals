5cb0e582d0daf954da2b823c10e9068f562f3003
siddharthkumra@vinsol.com <siddharthkumra@vinsol.com>
2019-11-07 16:25:56 +0530

    specials.js indentation fix

--- exercises/js/specials.js
+++ exercises/js/specials.js
@@ -1,33 +1,32 @@
-class DynamicSelect{
-  constructor(id){
-    this.$specials = $("#"+id);
+class DynamicSelect {
+  constructor(id) {
+    this.$specials = $("#" + id);
     this.cachedResponse;
     this.$target = $("<div/>");
     this.$specials.append(this.$target);
   }
-  addListeners(){
+  addListeners() {
     let $select = this.$specials.find("select");
     $select.change(this.getData.bind(this));
     this.$specials.find(".buttons").remove();
   }
-  getData(e){
+  getData(e) {
     let $option = $(e.currentTarget);
     let val = $option.val();
-    if(!this.cachedResponse){
-      $.get("/data/specials.json").done((res)=> {
-        this.cachedResponse=res;
+    if (!this.cachedResponse) {
+      $.get("/data/specials.json").done(res => {
+        this.cachedResponse = res;
         this.updateTarget(res[val]);
       });
-    }
-    else{
+    } else {
       this.updateTarget(this.cachedResponse[val]);
     }
   }
-  updateTarget(values){
+  updateTarget(values) {
     let html = "";
-    let title= "<h3>"+values.title+"</h3>";
-    let text = "<p>"+values.text+"</p>";
-    let image = "<img src=\""+values.image+"\"/>";
+    let title = "<h3>" + values.title + "</h3>";
+    let text = "<p>" + values.text + "</p>";
+    let image = '<img src="' + values.image + '"/>';
     html = title + text + image;
     this.$target.html(html);
     this.$target.css("color", values.color);
