8a7b8486d71a62f17418336f08b175142d508618
siddharthkumra@vinsol.com <siddharthkumra@vinsol.com>
2019-11-07 11:14:14 +0530

    tabs updated

--- exercises/js/tabs.js
+++ exercises/js/tabs.js
@@ -1,37 +1,33 @@
-//step1
-let $modules = $( "div.module" ).hide();
-//step2
-let $unorderedList = $( "<ul/> ");
-$unorderedList.insertBefore( "div.module:first" );
-//step3
-$modules
-  .each(function(idx, module){ 
-    let listItem = "<li>" + $( module ).find("h2").text() + "</li>"; 
-    $unorderedList.append(
-      $( listItem ).data("module", $( module ))
-    );
+class Tabs{
+  constructor(className) {
+    this.$modules = $("div."+className).hide();
+    this.$unorderedList = $( "<ul/> ");  
   }
-);
-//step4
-function clickHandler(){
-  let $elementClicked = $( this );
-  $elementClicked
-    .addClass("current")
-    .siblings()
-    .removeClass("current")
-    .each(function(idx, listItem){
-      $( listItem ).data("module").hide();
-      }
-    )
-    .end().data("module").show();
-};
-$unorderedList.find("li")
-  .each(function(idx, listItem){
-    $( listItem ).click(clickHandler);
+  createList(){
+    this.$unorderedList.insertBefore(this.$modules);
+    let that = this;
+    this.$modules.each(function(idx, module){ 
+      let $listItem = $("<li>" + $( module ).find("h2").text() + "</li>"); 
+      that.$unorderedList.append(
+        $listItem.data("module", $( module )).click(that.clickHandler.bind(that)));
+      });
+    }
+    clickHandler(e){
+      let $elementClicked = $(e.currentTarget);
+      $elementClicked
+      .addClass("current")
+      .siblings()
+      .removeClass("current")
+      .each(function(idx, listItem){
+        $( listItem ).data("module").hide();
+      })
+      .end().data("module").show();
+    };
+    createTabs(){
+      this.createList();
+      let $firstElementInList = this.$unorderedList.find("li").eq(0);
+      $firstElementInList.addClass("current").data("module").show();
+    }
   }
-);
-//step5
-let $firstElementInList = $unorderedList
-  .find("li").eq(0);
-$firstElementInList.data("module").show();
-$firstElementInList.addClass("current");
+  let tabbedModules = new Tabs("module");
+  tabbedModules.createTabs();
