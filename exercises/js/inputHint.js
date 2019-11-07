009d0eb102d7dec99e6e6d389d3992e9aa79fc89
siddharthkumra@vinsol.com <siddharthkumra@vinsol.com>
2019-11-07 10:13:35 +0530

    input hint updated

---
+++ exercises/js/inputHint.js
@@ -0,0 +1,26 @@
+class InputHint{
+  constructor(id){
+    this.$input = $( "input[name='" + id + "']" );
+    this.$label = $($( "label[for='"+id+"']"));
+    this.label_text = this.$label.text();
+  }
+  addHint(){
+    this.$input.val(this.label_text);
+    this.$input.addClass("hint").focus(this.onFocus.bind(this)).blur(this.onBlur.bind(this));
+    this.$label.remove();
+  }
+  onFocus(){
+    if(this.$input.val() === this.label_text && this.$input.hasClass("hint")){
+      this.$input.val("") ; 
+      this.$input.removeClass("hint");
+    }
+  }
+  onBlur(){
+     if(!this.$input.val()){
+       this.$input.val(this.label_text); 
+       this.$input.addClass("hint");
+      }
+  }
+}
+let input = new InputHint("q");
+input.addHint();
