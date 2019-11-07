c2c2c9542ec0e1e98c4cd95afd5d745a044e20d2
siddharthkumra@vinsol.com <siddharthkumra@vinsol.com>
2019-11-07 10:34:29 +0530

    slideshow updated

--- exercises/js/slideshow.js
+++ exercises/js/slideshow.js
@@ -1,89 +1,97 @@
-let $slideshow = $("#slideshow");
-let $items = $("#slideshow li").hide();
-let counter = 1;
-let totalItems = $items.length;
-
-let $counterContainer = $("<div> of  " +totalItems+" </div>").insertAfter($slideshow);
-let $counter = $("<b>"+counter+"</b>").prependTo($counterContainer);
-
-let $nextButton = $("<button id='nextButton'>Next</button>").click(buttonHandler);
-let $prevButton = $("<button id='prevButton'>Prev</button>").click(buttonHandler);
-let $startStopButton = $("<button id='startStopButton'>Stop</button>").click(buttonHandler);
-let buttonPressed = false, $currentSlide, delayTimeout;
-
-$nextButton.insertAfter($slideshow);
-$startStopButton.insertAfter($slideshow);
-$prevButton.insertAfter($slideshow);
-
-
 const delayInMs = 2000;
-
-// start the slide show
-setCurrentSlide();
-displaySlide();
-
-//to display the slide
-function displaySlide(){
-  $counter.text(counter);
-  $currentSlide.fadeIn("slow");
-  //handling the delay before it fades out
-  delayTimeout = setTimeout(() => {$currentSlide.fadeOut("slow"); dispSlideHandler()}, delayInMs);
-}
-
-//set the counter
-function setCounter(){
-  if($currentSlide){
-    if(!buttonPressed){
-      counter += 1;
+class Slideshow{
+  constructor(id) {
+    this.$currentSlide;
+    this.delayTimeout;
+    this.$counter;
+    this.$nextButton;
+    this.$prevButton;
+    this.$startStopButton;
+    this.$slideshow = $("#"+id);
+    this.$items = this.$slideshow.children("li");
+    this.counter = 1;
+    this.totalItems = this.$items.length;
+    this.buttonPressed = false;
+    
+  }
+  setupSlideshow(){
+    this.$items.hide();
+    
+    let $counterContainer = $("<div> of  " + this.totalItems+" </div>").insertAfter(this.$slideshow);
+    this.$counter = $("<b>"+this.counter+"</b>").prependTo($counterContainer);
+    
+    this.$nextButton = $("<button id='nextButton'>Next</button>").click(this.buttonHandler.bind(this));
+    this.$prevButton = $("<button id='prevButton'>Prev</button>").click(this.buttonHandler.bind(this));
+    this.$startStopButton = $("<button id='startStopButton'>Stop</button>").click(this.buttonHandler.bind(this));
+    
+    this.$nextButton.insertAfter(this.$slideshow);
+    this.$startStopButton.insertAfter(this.$slideshow);
+    this.$prevButton.insertAfter(this.$slideshow);
+  }
+  start(){
+    this.setupSlideshow();
+    this.setCurrentSlide();
+    this.displaySlide();
+  }
+  displaySlide(){
+    this.$counter.text(this.counter);
+    this.$currentSlide.fadeIn("slow");
+    //handling the delay before it fades out
+    this.delayTimeout = setTimeout(() => {this.$currentSlide.fadeOut("slow"); this.dispSlideHandler()}, delayInMs);
+  }
+  setCounter(){
+    if(this.$currentSlide){
+      if(!this.buttonPressed){
+        this.counter += 1;
+      }
+      else{
+        this.buttonPressed = false;
+      }
+      if(this.counter>this.totalItems){
+        this.counter = 1;
+      }
+      else if(this.counter<=0){
+        this.counter = this.totalItems;
+      }
+    }
+  }
+  setCurrentSlide(){
+    this.setCounter();
+    this.$currentSlide = $(this.$items[this.counter - 1]);
+  }
+  
+  //handle how the slide is displayed
+  dispSlideHandler(){
+    clearTimeout(this.delayTimeout);
+    this.$currentSlide.hide();
+    this.setCurrentSlide();
+    this.displaySlide();
+  }
+  buttonHandler(e){
+    let button = e.currentTarget;
+    this.buttonPressed = true;
+    if(!(button === this.$startStopButton.get(0))){
+      if(button === this.$nextButton.get(0)){
+        this.counter += 1;
+      }
+      else if(button === this.$prevButton.get(0)){
+        this.counter -= 1;
+      }
+      this.$startStopButton.text("Stop");
+      this.dispSlideHandler(); 
     }
     else{
-      buttonPressed = false;
-    }
-    if(counter>totalItems){
-      counter = 1;
-    }
-    else if(counter<=0){
-      counter = totalItems;
+      if(this.$startStopButton.text() === "Stop"){
+        clearTimeout(this.delayTimeout);
+        this.$startStopButton.text("Start");
+      }
+      else{
+        this.$startStopButton.text("Stop");
+        this.dispSlideHandler();
+      }
     }
   }
 }
 
-//set the current slide element
-function setCurrentSlide(){
-  setCounter();
-  $currentSlide = $($items[counter - 1]);
-}
-
-//handle how the slide is displayed
-function dispSlideHandler(){
-  clearTimeout(delayTimeout);
-  $currentSlide.hide();
-  setCurrentSlide();
-  displaySlide();
-}
-
-//handles the next, prev and stop button
-function buttonHandler(){
-  let button = this;
-  buttonPressed = true;
-  if(!(button === $startStopButton.get(0))){
-    if(button === $nextButton.get(0)){
-      counter += 1;
-    }
-    else if(button === $prevButton.get(0)){
-      counter -= 1;
-    }
-    $startStopButton.text("Stop");
-    dispSlideHandler(); 
-  }
-  else{
-    if($startStopButton.text() === "Stop"){
-      clearTimeout(delayTimeout);
-      $startStopButton.text("Start");
-    }
-    else{
-      $startStopButton.text("Stop");
-      dispSlideHandler();
-    }
-  }
-}
+let slideshow = new Slideshow("slideshow");
+slideshow.start();
