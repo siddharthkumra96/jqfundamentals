0efc473ebe5c5ca6a003afd0b50c85512d7d8f4f
siddharthkumra@vinsol.com <siddharthkumra@vinsol.com>
2019-11-07 16:24:27 +0530

    navigation.js indentation fix

--- exercises/js/navigation.js
+++ exercises/js/navigation.js
@@ -1,21 +1,33 @@
-class DropDownNavigation{
-  constructor(id){
-    this.$navEls = $("#"+id+" li");
+class DropDownNavigation {
+  constructor(id) {
+    this.$navEls = $("#" + id + " li");
   }
-  addHover(){
-    this.$navEls.hover(this.hoverHandler.bind(this), this.callbackHover.bind(this)).click(this.clickHandler.bind(this));
+  addHover() {
+    this.$navEls
+      .hover(this.hoverHandler.bind(this), this.callbackHover.bind(this))
+      .click(this.clickHandler.bind(this));
   }
-  hoverHandler(e){
+  hoverHandler(e) {
     let $currentEl = $(e.currentTarget);
-    $currentEl.addClass("hover").find("ul").show();
+    $currentEl
+      .addClass("hover")
+      .find("ul")
+      .show();
   }
-  callbackHover(e){
-    let $curEl = $(e.currentTarget); 
-    $curEl.removeClass("hover").find("ul").hide();
+  callbackHover(e) {
+    let $curEl = $(e.currentTarget);
+    $curEl
+      .removeClass("hover")
+      .find("ul")
+      .hide();
   }
-  clickHandler(e){
+  clickHandler(e) {
     e.stopPropagation();
-    window.location.assign($(e.currentTarget).find("a").attr("href"));
+    window.location.assign(
+      $(e.currentTarget)
+        .find("a")
+        .attr("href")
+    );
   }
 }
 var navDropDown = new DropDownNavigation("nav");
