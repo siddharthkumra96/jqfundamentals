7bd38b5233684b6211a774ccc3b5b5d02e1b1d67
siddharthkumra@vinsol.com <siddharthkumra@vinsol.com>
2019-11-14 14:35:17 +0530

    ex3 updated

--- exercises/js/ex3.js
+++ exercises/js/ex3.js
@@ -1,13 +1,14 @@
-//Q1
-for( let i = 8; i<13; i++){
-  let listElement = "<li> List Item "+i+"</li>";
-  $( listElement ).appendTo( "#myList" );
+// Q1
+let listItems = '';
+for (let i = 8; i < 13; i++) {
+  listItems += `<li> List Item ${i} </li>`;
 }
-//Q2
-let q2 = $( "#myList :nth-child(odd)" ).remove();
-//Q3
-let q3 = $( "div.module:last" ).append("<h2>Hello</h2><p>World</p>");
-//Q4
-let q4 = $( "#specials select" ).append("<option value=\"Wednesday\">Wednesday</option>");
-//Q5
-let q5 = $( "div.module:last" ).after( $("<div class='module'></div>").append( $( "img[alt='fruit']" ).clone()));
+$('#myList').append($(listItems));
+// Q2
+const q2 = $('#myList :nth-child(odd)').remove();
+// Q3
+const q3 = $('div.module:last').append('<h2>Hello</h2><p>World</p>');
+// Q4
+const q4 = $('#specials select').append('<option value="Wednesday">Wednesday</option>');
+// Q5
+const q5 = $('div.module:last').after($("<div class='module'></div>").append($("img[alt='fruit']").clone()));
