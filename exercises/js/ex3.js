6392cb1550d4c6c05f45b3c6bc3223cedca581b4
siddharthkumra@vinsol.com <siddharthkumra@vinsol.com>
2019-11-14 14:36:15 +0530

    ex3 updated

--- exercises/js/ex3.js
+++ exercises/js/ex3.js
@@ -7,8 +7,13 @@ $('#myList').append($(listItems));
 // Q2
 const q2 = $('#myList :nth-child(odd)').remove();
 // Q3
-const q3 = $('div.module:last').append('<h2>Hello</h2><p>World</p>');
+const q3 = $('div.module:last')
+  .append($('<h2 />', { text: 'Hello' }))
+  .append($('<p />', { text: 'World' }));
 // Q4
-const q4 = $('#specials select').append('<option value="Wednesday">Wednesday</option>');
+const q4 = $('#specials select').append($('<option />', { value: 'Wednesday', text: 'Wednesday' }));
 // Q5
-const q5 = $('div.module:last').after($("<div class='module'></div>").append($("img[alt='fruit']").clone()));
+const q5 = $('div.module:last').after(
+  $('<div />', { className: 'module' })
+    .append($("img[alt='fruit']").clone())
+);
